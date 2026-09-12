c1a12e06f04aa591535506a4a028fbd2bc5762e3
Sachin Tajane <sachintajane@Sachins-MacBook-Pro.local>
2026-09-12 20:25:44 +0530

    ci: configure Next.js static export and GitHub Actions automated deployment for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 export default nextConfig;
+
